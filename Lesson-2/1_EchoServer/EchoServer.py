e0a55fb8d0ade94b185b647c5d46488a660a752f
thangthin <thangthin@gmail.com>
2017-06-04 16:32:10 -0500

    echo server

--- Lesson-2/1_EchoServer/EchoServer.py
+++ Lesson-2/1_EchoServer/EchoServer.py
@@ -28,8 +28,11 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
 
+        # get path
+        path = self.path.lstrip("/");
         # Now, write the response body.
-        self.wfile.write("Hello, HTTP!\n".encode())
+        body = "%s \n" % (path)
+        self.wfile.write(body.encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)  # Serve on all addresses, port 8000.
